e3364213877246f8e7fbd85c52951bbecb9b5af8
JoeZhou1234 <mr.joezhou@gmail.com>
2022-12-12 14:21:36 -0500

    Completed TwoSum

---
+++ src/com/company/TwoSum.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] output = {-1, -1};
+        int arrlen = nums.length;
+        for (int i = 0; i < arrlen - 1; i++) {
+            for (int j = i + 1; j < arrlen; j++) {
+                if (nums[i] + nums[j] == target) {
+                    output[0] = i;
+                    output[1] = j;
+                    return output;
+                }
+            }
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+//        Output should be [0,1]
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] output = twoSum(nums, target);
+        System.out.println(Arrays.toString(output));
+
+//        Output should be [1,2]
+        nums = new int[]{3, 2, 4};
+        target = 6;
+        output = twoSum(nums, target);
+        System.out.println(Arrays.toString(output));
+
+//        Output should be [0,1]
+        nums = new int[]{3, 3};
+        target = 6;
+        output = twoSum(nums, target);
+        System.out.println(Arrays.toString(output));
+    }
+}
